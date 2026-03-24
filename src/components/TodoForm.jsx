584b86cc2e2d4057450aefe497a285c42431a4be
Your Name <ellycreativity8@gmail.com>
2026-03-24 13:03:25 +0200

    here I have added form to submit

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 
 
-function TodoForm(props) {
+function TodoForm() {
+  
+
     return (
         <div>
-            
+              <form action="" method="post">
+                 <h1>Add Items</h1>
+                  <input type="text" placeholder="Add New Item here" name="values" />
+                  <button type="button">Add New Item</button>
+
+              </form>
         </div>
     );
 }
