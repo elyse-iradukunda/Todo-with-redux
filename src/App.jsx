e7c2429bbfec76c9f27efc05ef514b725007419c
Your Name <ellycreativity8@gmail.com>
2026-03-24 13:27:09 +0200

    simple todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+
 export default function App() {
   return (
     <>
-     
+       <TodoList/>
+       <TodoForm/>
+       
     </>
   );
 }
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -3,15 +3,32 @@ import { useState } from "react";
 
 function TodoForm() {
   
+    const [task,setTasks] = useState([]);
+
+        const handleSubmit=(e)=>{
+            e.preventDefault();
+           
+            const values = e.target.task.value;
+
+            setTasks([...task, values ]);
+        }
 
     return (
         <div>
-              <form action="" method="post">
+              <form action="" onSubmit={handleSubmit} method="post">
                  <h1>Add Items</h1>
-                  <input type="text" placeholder="Add New Item here" name="values" />
-                  <button type="button">Add New Item</button>
-
+                  <input type="text" placeholder="Add New Item here" name="task" />
+                  <button type="submit">Add New Item</button>
               </form>
+                
+                <div>
+                     <ul>
+                         { task.map((elm,i)=>{
+                            return <li key={i}>{elm}</li>
+                         })}
+                     </ul>
+                </div>
+
         </div>
     );
 }
