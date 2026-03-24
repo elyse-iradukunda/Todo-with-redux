0dbc373c8557c5043a8c28f085921678ebaee6e6
Your Name <ellycreativity8@gmail.com>
2026-03-24 14:08:35 +0200

    here is  about my new todo without redux

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,36 +1,51 @@
 import { useState } from "react";
-
+import "../index.css";
 
 function TodoForm() {
-  
-    const [task,setTasks] = useState([]);
+  const [task, setTasks] = useState([]);
 
-        const handleSubmit=(e)=>{
-            e.preventDefault();
-           
-            const values = e.target.task.value;
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-            setTasks([...task, values ]);
-        }
+    const values = e.target.task.value;
+    if (!values) return;
 
-    return (
-        <div>
-              <form action="" onSubmit={handleSubmit} method="post">
-                 <h1>Add Items</h1>
-                  <input type="text" placeholder="Add New Item here" name="task" />
-                  <button type="submit">Add New Item</button>
-              </form>
-                
-                <div>
-                     <ul>
-                         { task.map((elm,i)=>{
-                            return <li key={i}>{elm}</li>
-                         })}
-                     </ul>
-                </div>
+    setTasks([...task, values]);
+    e.target.reset();
+  };
 
-        </div>
-    );
+  const handleDelete = (index)=>{
+       
+    const del = task.filter((_,i)=> i!==index);
+    setTasks(del);
+  }
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit} className="form">
+        <h1>Add Items</h1>
+
+        <input
+          type="text"
+          placeholder="Add New Item here"
+          name="task"
+          className="input"
+        />
+
+        <button type="submit" className="button">
+          Add New Item
+        </button>
+      </form>
+
+      <div className="list-container">
+        <ul>
+          {task.map((elm, i) => {
+            return <li key={i} className="list-item">{elm} <button type="button" onClick={()=>handleDelete(i)}>Delete</button> </li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default TodoForm;
